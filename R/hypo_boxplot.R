0d35698320470839d229b2be86187d7022af296a
Karl Broman <kbroman@gmail.com>
2013-03-01 11:04:01 -0600

    Revise labels in static box plot figure

--- R/hypo_boxplot.R
+++ R/hypo_boxplot.R
@@ -7,10 +7,10 @@ hypo.mlratio <- hypo.mlratio[,order(apply(hypo.mlratio, 2, function(a) median(a[
 png("../figs/manyboxplots.png", width=1000, height=600, pointsize=18, bg="black")
 par(bg="black", fg="white", col="white", col.axis="white", col.lab="white", mar=c(3.1, 3.6, 1.1, 1.1))
 manyboxplot(hypo.mlratio, dotcol=NA, c(0.01, 0.05, 0.1, 0.25), ylim=c(-1, 1), hlines=seq(-1, 1, by=0.25),
-            vlines=seq(100, 500, by=100), ylab="Quantile",
+            vlines=seq(100, 500, by=100), ylab="Gene expression",
             mgp=c(2.1, 0.35, 0), xlab="")
 lines(1:ncol(hypo.mlratio), apply(hypo.mlratio, 2, median, na.rm=TRUE), col="blue", lwd=2)
-title(xlab="Mouse", mgp=c(1.9, 0.35, 0))
+title(xlab="Arrays, sorted by median expression", mgp=c(1.9, 0.35, 0))
 plim <- par("usr")
 rect(plim[1], plim[3], plim[2], plim[4], col=NA)
 dev.off()
