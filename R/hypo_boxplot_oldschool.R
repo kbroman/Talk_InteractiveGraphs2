d8cf3e993290cdf7734b95d37fd10d9d4618467a
Karl Broman <kbroman@gmail.com>
2013-03-07 09:11:50 -0600

    Change color and size of outlier points in "old school" boxplot

--- R/hypo_boxplot_oldschool.R
+++ R/hypo_boxplot_oldschool.R
@@ -17,7 +17,7 @@ dev.off()
 
 png("../figs/manyboxplots_oldschoolB.png", width=1000, height=600, pointsize=18, bg=bgcolor)
 par(bg=bgcolor, fg="white", col="white", col.axis="white", col.lab="white", mar=c(4.1, 3.6, 1.1, 1.1))
-boxplot(hypo.mlratio, las=1, cex=0.5, names=rep("", n.arrays), outcol="darkslateblue")
+boxplot(hypo.mlratio, las=1, cex=0.4, names=rep("", n.arrays), outcol=rgb(170,170,170,maxColorValue=255))
 axis(side=1, at=seq(100, n.arrays, by=100), tick=FALSE, mgp=c(1.8, 0.8, 0))
 title(xlab="Arrays, sorted by median expression", mgp=c(1.8, 0.35, 0))
 title(ylab="Gene expression", mgp=c(1.8, 0.35, 0))
